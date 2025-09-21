31b94b1b3a2c3cb07dc5dce5fc81b2bff8fc005d
hisham-112 <aminhishamharuna@gmail.com>
2025-09-21 18:19:21 +0000

    hello world

---
+++ js/assignment.js/gibson.js
@@ -0,0 +1,73 @@
+
+// function capitalizeWords(sentence) {
+//   alert(sentence);
+
+//   let result = sentence
+//       .split(" ") // break into words
+//       .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // capitalize first letter
+//       .join(" "); // join them back together
+
+//   return result;
+// }
+
+// capitalizeWords("hello hisham");
+
+
+
+
+/*const numbers1 = [4, 5, 6];
+
+const numbers2 = numbers1
+.map(value => value * 2)
+.filter(value => value % 2 === 0);
+
+console.log(numbers2); // [8, 10, 12] 
+                       // [4,6]*/
+
+
+
+
+
+/*const user = {
+  id: 1,
+  profile: {
+    username: "mandem",
+    email: "mandem@example.com"
+  },
+  settings: {
+    theme: "dark",
+    notifications: true
+  }
+};
+
+function printUserInfo({ profile: { username, email }, settings: { theme } }) {
+  console.log("Username:", username);
+  console.log("Email:", email);
+  console.log("Theme:", theme);
+}
+
+printUserInfo(user);*/
+
+
+
+
+
+
+
+// const getAffordableProducts = (products, budget) => {
+   
+//   const affordable = products.filter(product => product.price <= budget );
+
+//    console.log(affordable);
+
+// };
+// const products = [
+//     { name: "Laptop", price: 1200 },
+//     { name: "Phone", price: 800 },
+//     { name: "Tablet", price: 600 },
+//   ];
+
+// getAffordableProducts(products,800);
+
+
+
